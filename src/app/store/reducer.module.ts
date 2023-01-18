6a66da574394d2974e9128d68336dc2c83ca0e69
DESKTOP-IASUES3\abe <barbozabraham@gmail.com>
2023-01-18 13:33:53 -0800

    messing with follow service

--- src/app/store/reducer.module.ts
+++ src/app/store/reducer.module.ts
@@ -4,9 +4,11 @@ import { ActionReducer, ActionReducerMap, StoreModule } from '@ngrx/store';
 import { localStorageSync } from 'ngrx-store-localstorage';
 
 import { authReducer } from './reducers/auth.reducer';
+import { followReducer } from './reducers/follows.reducer';
 
 export const appReducers = {
   auth: authReducer,
+  follow: followReducer,
 } as ActionReducerMap<{}>;
 
 export function localStorageSyncReducer(
