d3a3c24a9d09dc3bf96c123a79073e251dd38517
SethEvry <92412142+SethEvry@users.noreply.github.com>
2023-01-24 18:00:09 -0500

    fix notification if unread is empty

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import Notification from 'src/app/models/Notification';
 import { AuthService } from 'src/app/services/auth.service';
 import { NotificationService } from 'src/app/services/notification.service';
 import {
@@ -22,7 +23,7 @@ export class NavbarComponent implements OnInit {
   public preferences$: Observable<PreferencesState>;
   public auth$: Observable<AuthState>;
   public isExpanded: boolean = false;
-  public notes$: any;
+  public notes$: Observable<Notification[]>;
 
   constructor(
     private notiService: NotificationService,
